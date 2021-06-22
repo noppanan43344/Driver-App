0bc685d3b5176a2ef2a7c7c901c6aa6adfe2727d
noppanan43344 <noppanan.lert@gmail.com>
2021-06-22 21:24:37 +0700

    report 4 items

--- src/navigations/0-Home/Screen/Report/ReportScreen.js
+++ src/navigations/0-Home/Screen/Report/ReportScreen.js
@@ -6,20 +6,13 @@ import {
     ScrollView,
     TouchableHighlight,
     Alert,
+    Image,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { FONT_BOLD, FONT_SIZES, COLORS, FONT_MED } from '@components/styles';
 import Header from '@components/Header';
 import BackButton from '@components/Button/BackButton';
 export default function ReportScreen(props) {
-    createTwoButtonAlert = () =>
-        Alert.alert('Alert Title', 'My Alert Msg', [
-            {
-                text: 'Cancel',
-                onPress: () => console.log('Cancel Pressed'),
-                style: 'cancel',
-            },
-            { text: 'OK', onPress: () => console.log('OK Pressed') },
-        ]);
     return (
         <ScrollView>
             <Header
@@ -28,12 +21,110 @@ export default function ReportScreen(props) {
                     <BackButton onPress={() => props.navigation.goBack()} />
                 }
             />
-            <View style={{ marginTop: 20, paddingHorizontal: 15 }}>
-                <TouchableHighlight
-                    underlayColor="null"
-                    onPress={() => {
-                        createTwoButtonAlert();
-                    }}>
+
+            <View
+                style={{
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <View
+                        style={{
+                            flex: 0.5,
+                            alignItems: 'center',
+                        }}>
+                        <TouchableHighlight
+                            underlayColor="null"
+                            onPress={() =>
+                                props.navigation.navigate('JobScreen')
+                            }>
+                            <View style={styles.boxs}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Icon
+                                        name="product-hunt"
+                                        size={40}
+                                        color="red"
+                                        style={{ marginRight: 15 }}
+                                    />
+                                    <Text style={styles.fontIcon}>1/20</Text>
+                                </View>
+                                <Text style={styles.fontRed}>
+                                    สินค้าที่ส่งแล้ว
+                                </Text>
+                            </View>
+                        </TouchableHighlight>
+                        <TouchableHighlight
+                            underlayColor="null"
+                            onPress={() =>
+                                props.navigation.navigate('TopUpHistoryScreen')
+                            }>
+                            <View style={styles.boxs}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Icon
+                                        name="clock"
+                                        size={40}
+                                        color="orange"
+                                        style={{ marginRight: 10 }}
+                                    />
+                                    <Text style={styles.fontIcon}>
+                                        1ชั่วโมง 30นาที
+                                    </Text>
+                                </View>
+                                <Text style={styles.fontOrange}>เวลาเดินทาง</Text>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+
+                    <View
+                        style={{
+                            flex: 0.5,
+                            alignItems: 'center',
+                        }}>
+                        <TouchableHighlight
+                            underlayColor="null"
+                            onPress={() =>
+                                props.navigation.navigate('JobScreen')
+                            }>
+                            <View style={styles.boxs}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Icon
+                                        name="map"
+                                        size={40}
+                                        color="blue"
+                                        style={{ marginRight: 10 }}
+                                    />
+                                    <Text style={styles.fontIcon}>
+                                        35 กิโลเมตร
+                                    </Text>
+                                </View>
+                                <Text style={styles.fontBlue}>ระยะทาง</Text>
+                            </View>
+                        </TouchableHighlight>
+                        <TouchableHighlight
+                            underlayColor="null"
+                            onPress={() =>
+                                props.navigation.navigate('TopUpHistoryScreen')
+                            }>
+                            <View style={styles.boxs}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Icon
+                                        name="dollar-sign"
+                                        size={40}
+                                        color="green"
+                                        style={{ marginRight: 10 }}
+                                    />
+                                    <Text style={styles.fontIcon}>
+                                        120.5 บาท
+                                    </Text>
+                                </View>
+                                <Text style={styles.fontGreen}>จำนวนเงิน</Text>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+            </View>
+            <View style={{ marginTop: 20, paddingHorizontal: 10 }}>
+                <TouchableHighlight underlayColor="null" onPress={() => {}}>
                     <View style={styles.box}>
                         <View style={styles.row}>
                             <View style={styles.flexStart}>
@@ -62,6 +153,9 @@ export default function ReportScreen(props) {
     );
 }
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     box: {
         backgroundColor: '#FFF',
         width: '100%',
@@ -70,6 +164,16 @@ const styles = StyleSheet.create({
         marginBottom: 15,
         elevation: 5,
         borderRadius: 10,
+    },
+    boxs: {
+        backgroundColor: '#FFF',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 15,
+        elevation: 5,
+        borderRadius: 10,
+        marginTop: 15,
+        width: 160,
     },
     font: {
         fontFamily: FONT_MED,
@@ -89,7 +193,6 @@ const styles = StyleSheet.create({
     row: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        // alignItems: 'center',
         paddingHorizontal: 20,
     },
     flexStart: {
@@ -98,4 +201,38 @@ const styles = StyleSheet.create({
         marginVertical: 5,
     },
     flexEnd: { flex: 0.5, alignItems: 'flex-end', marginVertical: 5 },
+    imageIcon: {
+        height: 80,
+        width: 80,
+    },
+    fontIcon: {
+        marginTop: 10,
+        fontFamily: FONT_MED,
+        fontSize: FONT_SIZES['400'],
+        color: 'black',
+    },
+    fontRed: {
+        marginTop: 10,
+        fontFamily: FONT_BOLD,
+        fontSize: FONT_SIZES['500'],
+        color: 'red',
+    },
+    fontBlue: {
+        marginTop: 10,
+        fontFamily: FONT_BOLD,
+        fontSize: FONT_SIZES['500'],
+        color: 'blue',
+    },
+    fontGreen: {
+        marginTop: 10,
+        fontFamily: FONT_BOLD,
+        fontSize: FONT_SIZES['500'],
+        color: 'green',
+    },
+    fontOrange: {
+        marginTop: 10,
+        fontFamily: FONT_BOLD,
+        fontSize: FONT_SIZES['500'],
+        color: 'orange',
+    },
 });
